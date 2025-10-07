d894c40750f6a8da72d2991d5ec2ac23e0eb9126
dieatmore <s.d.wang.1@nefu.edu.cn>
2025-10-07 11:19:05 +0800

    first commit

---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>login</h1>
+  <el-button type="primary">测试 Element Plus 按钮</el-button>
+  <div class="bg-purple-500">测试 Tailwind 样式</div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
